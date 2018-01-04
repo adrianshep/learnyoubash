aec823e8c05b0edfc0ccf502e2a3e316093147c5
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-01-04 09:27:07 -0500

    2nd lesson in learnyoubash

--- 2-variables.bash
+++ 2-variables.bash
@@ -73,3 +73,47 @@
    » To execute your program, run: learnyoubash run program.bash
    » To verify your program, run: learnyoubash verify program.bash
    » For help run: learnyoubash help
+
+   Here's the official solution in case you want to compare notes:
+
+─────────────────────────────────────────────────────────────────────────────
+
+    #!/usr/bin/env bash
+
+    echo "User $USER in directory $PWD."
+
+─────────────────────────────────────────────────────────────────────────────
+
+    #!/usr/bin/env bash
+
+    echo "User $USER in directory $PWD."
+
+
+# Awesome!
+
+ Okay, you've done this!
+
+ Variables are a very important part of any programming language and now
+ you know how they work in Bash.
+
+ In this exercise you used the $PWD variable. In addition, there is also
+ the pwd command which returns the same thing as the $PWD variable, the
+ present working directory. So remember, when you need to get the current
+ directory name, use either the pwd command or the $PWD variable:
+
+    pwd        #> /Users/username/learnyoubash/variables/
+    echo $PWD  #> /Users/username/learnyoubash/variables/
+
+ Above you may notice special strings which start with the # sign. Do you
+ know what they are? They're comments.
+
+ Comments are special statements ignored by the shell interpreter. They
+ begin with a # symbol and continue on to the end of the line.
+
+ For example:
+
+    #!/bin/bash
+    # This script will print your username.
+    whoami
+
+ Use comments to explain what your script does and why.
