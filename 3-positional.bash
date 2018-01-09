475d2ab470ec5a610009596a2ab8ab5aa550033f
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-01-09 09:41:51 -0500

    3rd lesson in learnyoubash

--- 3-positional.bash
+++ 3-positional.bash
@@ -67,3 +67,48 @@
    » To execute your program, run: learnyoubash run program.bash
    » To verify your program, run: learnyoubash verify program.bash
    » For help run: learnyoubash help
+
+
+   Here's the official solution in case you want to compare notes:
+
+─────────────────────────────────────────────────────────────────────────────
+
+    #!/usr/bin/env bash
+
+    echo "1: $1"
+    echo "3: $3"
+    echo "5: $5"
+
+─────────────────────────────────────────────────────────────────────────────
+
+    #!/usr/bin/env bash
+
+    echo "1: $1"
+    echo "3: $3"
+    echo "5: $5"
+
+
+# Congrats!
+
+ Positional parameters will be very useful for building your own command
+ line applications.
+
+ For example, we have the script:
+
+    #!/usr/bin/env bash
+
+    echo "Arguments:"
+    echo $*
+
+ Run it with some flags:
+
+    ./script.bash --test -t -a -b -c
+
+ It will print:
+
+    Arguments:
+    --test -t -a -b -c
+
+ That means we can handle these flags and change what the script will
+ return. Exactly the same as with the --help flag, which outputs a help
+ message instead of running the program.
