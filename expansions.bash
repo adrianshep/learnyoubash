d601509ba0ee4ca916aa30edba2532983a470a1e
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-01-19 08:18:15 -0500

    5th lesson in learnyoubash

--- expansions.bash
+++ expansions.bash
@@ -1,3 +1,5 @@
-$RESULT=(( $1 * ($2 + $3) ))
+#$RESULT=(( $1 * ($2 + $3) ))
+R=(( ($3 + $2) * $1))
 
-echo project-$RESULT/{dest/{index.js,util.js},src/{index.js,util.js},test/{index.js,util.js}} 
+#echo project-$RESULT/{dest/{index.js,util.js},src/{index.js,util.js},test/{index.js,util.js}}
+echo project-$R/{src,dest,test}/{index,util}.js
