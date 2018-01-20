802b0b83472563bbedc755281521f7e4c2bb0f4e
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-01-20 09:52:58 -0500

    5th lesson in learnyoubash

--- 5-expansions.bash
+++ 5-expansions.bash
@@ -78,3 +78,40 @@
    » To execute your program, run: learnyoubash run program.bash
    » To verify your program, run: learnyoubash verify program.bash
    » For help run: learnyoubash help
+
+   Here's the official solution in case you want to compare notes:
+
+─────────────────────────────────────────────────────────────────────────────
+
+   #!/usr/bin/env bash
+
+   R=$(( ($3 + $2) * $1))
+   echo project-$R/{src,dest,test}/{index,util}.js
+
+─────────────────────────────────────────────────────────────────────────────
+
+   #!/usr/bin/env bash
+
+   R=$(( ($3 + $2) * $1))
+   echo project-$R/{src,dest,test}/{index,util}.js
+
+
+# Nice job!
+
+You just output the folder structure, but actually you can easily create
+this tree in the same way. Say hello to the mkdir and touch commands.
+
+The mkdir command create an empty folder with a given name. The touch
+command make an empty file with a given name.
+
+So now, knowing about these commands, we can do something like this:
+
+   mkdir -p project/{src,dest,test}/
+   touch project/{src,dest,test}/{index,util}.js
+
+Above we use the -p flag with the mkdir command to make parent directories
+as needed.
+
+To see the content of the folder use the ls command.
+
+Run man to get to know more about the mkdir, touch and ls commands.
