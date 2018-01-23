a7d33b113b89231a78cd8de425891230224e199b
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-01-23 09:33:55 -0500

    6th lesson in learnyoubash

---
+++ lists.bash
@@ -0,0 +1,5 @@
+Using lists of commands (&, &&, || or ;) output First parameter is false.
+if the first positional parameter is false, the path to the current
+directory if the second parameter is true; if third parameter is true
+output the list of files in the current directory or Third parameter is
+false. if it is false.
