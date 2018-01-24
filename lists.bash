a8a917de3d0a4a5bd98a6963cc09f446e9369b46
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-01-24 10:49:32 -0500

    6th lesson in learnyoubash

--- lists.bash
+++ lists.bash
@@ -1,5 +1,11 @@
-Using lists of commands (&, &&, || or ;) output First parameter is false.
-if the first positional parameter is false, the path to the current
-directory if the second parameter is true; if third parameter is true
-output the list of files in the current directory or Third parameter is
-false. if it is false.
+#  Using lists of commands (&, &&, || or ;) output First parameter is false.
+#  if the first positional parameter is false, the path to the current
+#  directory if the second parameter is true; if third parameter is true
+#  output the list of files in the current directory or Third parameter is
+#  false. if it is false.
+
+$1 || "First parameter is false."
+
+$2 && pwd
+
+$3 && ls || "Third parameter is false."
