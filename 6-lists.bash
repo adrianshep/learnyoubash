d2b80891764953c9f02dad11b2229540d20798c7
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-01-25 15:10:28 -0500

    6th lesson in learnyoubash

--- 6-lists.bash
+++ 6-lists.bash
@@ -125,4 +125,45 @@
    » To print these instructions again, run: learnyoubash print
    » To execute your program, run: learnyoubash run program.bash
    » To verify your program, run: learnyoubash verify program.bash
-   » For help run: learnyoubash help 
+   » For help run: learnyoubash help
+
+   Here's the official solution in case you want to compare notes:
+
+  ─────────────────────────────────────────────────────────────────────────────
+
+      #!/usr/bin/env bash
+
+      $1 || echo "First parameter is false."
+
+      $2 && pwd
+
+      $3 && ls || echo "Third parameter is false."
+
+  ─────────────────────────────────────────────────────────────────────────────
+
+      #!/usr/bin/env bash
+
+      $1 || echo "First parameter is false."
+
+      $2 && pwd
+
+      $3 && ls || echo "Third parameter is false."
+
+
+  # Great!
+
+   Streams and pipes are useful to create logs and to transfer data from one
+   command to another. Lists of commands give you the opportunity to change
+   the result of the execution of your script.
+
+   You are already familiar with the ls command. But what if you need to list
+   all files with a specific extension in the current directory?
+
+   Meet the grep command! The grep command prints lines matching a pattern.
+   Now, using grep we can solve the problem like so:
+
+      ls | grep .md$
+
+   The pipeline above will print only files with .md extension.
+
+   Learn more about grep using man grep.
