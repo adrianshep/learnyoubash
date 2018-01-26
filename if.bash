a33562ee0c370c738da6b42f52f563329e3a585e
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-01-26 10:32:45 -0500

    7th lesson in learnyoubash

---
+++ if.bash
@@ -0,0 +1,22 @@
+## THE CHALLENGE
+
+#   Create a file named if.bash.
+
+#   Using if statements and primaries, output Good morning! if the first
+#   positional parameter is less than 12. Output Good afternoon! if it is
+#   equal to/greater than 12 but less than 18. Otherwise, output Good evening!
+#   if it is equal to/greater than 18. Take care about cases when the
+#   positional argument is less than 0 and greater than 24 (print Error! in
+#   these cases).
+
+#   For example:
+
+#      ./if.bash -5
+#      ./if.bash 12
+#      ./if.bash 21
+
+#   Output:
+
+#      Error!
+#      Good afternoon!
+#      Good evening!
