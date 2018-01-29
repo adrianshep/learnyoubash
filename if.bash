bd17c7216b9aaaaca1f3a870220d2e6233473e9a
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-01-29 09:32:14 -0500

    7th lesson in learnyoubash

--- if.bash
+++ if.bash
@@ -21,12 +21,22 @@
 #      Good afternoon!
 #      Good evening!
 
-if [[ $1 -lt 0 ] -o [ $1 -gt 24 ]]; then
-  echo "Error!"
-elif [[ $1 -lt 12 ]]; then
+#  if [[ $1 -lt 0 ] -o [ $1 -gt 24 ]]; then
+#    echo "Error!"
+#  elif [[ $1 -lt 12 ]]; then
+#    echo "Good morning!"
+#  elif [[ $1 -ge 12 ] -a [$1 -lt 18]]; then
+#    echo "Good afternoon!"
+#  else
+#    echo "Good evening!"
+#  fi
+
+if [[ $1 -ge 0 && $1 -lt 12 ]]; then
   echo "Good morning!"
-elif [[ $1 -ge 12 ] -a [$1 -lt 18]]; then
+elif [[ $1 -ge 12 && $1 -lt 18 ]]; then
   echo "Good afternoon!"
-else
+elif [[ $1 -ge 18 && $1 -lt 24 ]]; then
   echo "Good evening!"
+else
+  echo "Error!"
 fi
