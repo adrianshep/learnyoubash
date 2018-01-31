2206107e8cbce01575fdd73baf9d9c2db3cae21f
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-01-31 09:36:03 -0500

    8th lesson in learnyoubash

---
+++ case.bash
@@ -0,0 +1,13 @@
+#  case "$FRUIT" in
+#    (apple)
+#      echo 'Mmmmh... I like apple!'
+#      ;;
+#    (banana)
+#      echo 'Hm, a bit awry, no?'
+#      ;;
+#    (orange|tangerine)
+#      echo "I don't like it!" && exit 1
+#    ;;
+#    (*)
+#    ;;
+#  esac
