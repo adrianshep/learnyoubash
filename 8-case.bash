5980c7c65b27a90dfdddebcd32feed5427785538
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-02-02 11:27:52 -0500

    8th lesson in learnyoubash

--- 8-case.bash
+++ 8-case.bash
@@ -54,4 +54,87 @@
   » To print these instructions again, run: learnyoubash print
   » To execute your program, run: learnyoubash run program.bash
   » To verify your program, run: learnyoubash verify program.bash
-  » For help run: learnyoubash help  
+  » For help run: learnyoubash help
+
+  Here's the official solution in case you want to compare notes:
+
+ ─────────────────────────────────────────────────────────────────────────────
+
+     #!/usr/bin/env bash
+
+     case "$1" in
+       ("jpg"|"jpeg")
+         echo "It is jpeg."
+       ;;
+       "png")
+         echo "It is png."
+       ;;
+       "gif")
+         echo "It is gif."
+       ;;
+       *)
+         echo "$1 is not an image!"
+       ;;
+     esac
+
+ ─────────────────────────────────────────────────────────────────────────────
+
+     #!/usr/bin/env bash
+
+     case "$1" in
+       ("jpg"|"jpeg")
+         echo "It is jpeg."
+       ;;
+       "png")
+         echo "It is png."
+       ;;
+       "gif")
+         echo "It is gif."
+       ;;
+       *)
+         echo "$1 is not an image!"
+       ;;
+     esac
+
+     ─────────────────────────────────────────────────────────────────────────────
+
+     incorrect solution:
+
+       case $EXT in
+         (jpeg)
+           echo 'It is jpeg.'
+           ;;
+         (png)
+           echo 'It is png.'
+           ;;
+         (gif)
+           echo 'It is gif.'
+           ;;
+         (*)
+           echo '[EXT] is not an image!'
+           ;;
+         esac
+
+
+ # Cool!
+
+  As you have seen, using case is much more convenient when you are
+  confronted with a couple of different cases.
+
+  In this problem you were checking extensions. But how do we actually
+  access a directory or filename?
+
+  We already know about pwd command that return full path to current
+  directory. However there are also other similar commands, like basename
+  and dirname.
+
+  The basename command strips the directory and suffix from filenames such
+  as:
+
+     basename path/to/file.ext  #> file
+
+  The dirname command strips the last component from a file name, like this:
+
+     dirname /path/to/file.ext  #> path/to
+
+  Run man basename and man dirname to learn more about these cool commands.
