e11626d7d37bb83a66d201709067596060302382
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-02-02 11:28:19 -0500

    8th lesson in learnyoubash

--- case.bash
+++ case.bash
@@ -12,23 +12,23 @@
 #    ;;
 #  esac
 
-In this exercise you will create a small image extensions checker. Using a
-case statement, check if the first positional parameter is a jpeg (or
-jpg), png, or gif extension. Output It is jpeg., It is png., or It is gif.
-accordingly. Take care about cases when the positional argument contains
-any other extension (print [EXT] is not an image! in these cases).
+#  In this exercise you will create a small image extensions checker. Using a
+#  case statement, check if the first positional parameter is a jpeg (or
+#  jpg), png, or gif extension. Output It is jpeg., It is png., or It is gif.
+#  accordingly. Take care about cases when the positional argument contains
+#  any other extension (print [EXT] is not an image! in these cases).
 
-case $EXT in
-  (jpeg)
-    echo 'It is jpeg.'
-    ;;
-  (png)
-    echo 'It is png.'
-    ;;
-  (gif)
-    echo 'It is gif.'
-    ;;
-  (*)
-    echo '[EXT] is not an image!'
-    ;;
-  esac
+case "$1" in
+  ("jpg"|"jpeg")
+    echo "It is jpeg."
+  ;;
+  "png")
+    echo "It is png."
+  ;;
+  "gif")
+    echo "It is gif."
+  ;;
+  *)
+    echo "$1 is not an image!"
+  ;;
+esac
