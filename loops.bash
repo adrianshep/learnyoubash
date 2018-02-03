85ed9a7319f5b508951e8faf769a4b025a3909ee
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-02-03 10:04:04 -0500

    9th lesson in learnyoubash

---
+++ loops.bash
@@ -0,0 +1,20 @@
+## THE CHALLENGE
+
+#   Create a file named loops.bash.
+
+#   The first two positional parameters which will be passed into your script
+#   are the limits. You should write all even numbers in the range between the
+#   first and second positional parameters.
+
+#   Example:
+
+#      ./loops.bash 10 17
+
+#   Output:
+
+#      10
+#      12
+#      14
+#      16
+
+#   Use for, while or until loops to solve this problem.
